0bcba21891b1e188723534fe0446d84474ad2f4c
Chris Grimm <cgrimm1994@gmail.com>
2018-10-01 12:20:45 -0400

    modified baseline to support internal terminations

--- baselines/deepq/deepq.py
+++ baselines/deepq/deepq.py
@@ -290,8 +290,9 @@ def learn(env,
             if done:
                 obs = env.reset()
                 episode_rewards.append(0.0)
-                print(len(episode_rewards))
                 reset = True
+            if internal_done:
+                print(len(episode_rewards))
 
             if t > learning_starts and t % train_freq == 0:
                 # Minimize the error in Bellman's equation on a batch sampled from replay buffer.
